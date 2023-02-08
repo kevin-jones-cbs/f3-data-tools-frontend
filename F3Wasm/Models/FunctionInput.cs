20d9dbb1a09dceb9a62d279e358c5f27216c452b
Kevin Jones <kevin.jones@cbsinteractive.com>
2023-02-08 08:40:06 -0800

    Remove testing

--- F3Wasm/Models/FunctionInput.cs
+++ F3Wasm/Models/FunctionInput.cs
@@ -6,6 +6,6 @@ namespace F3Wasm.Models
         public List<Pax> Pax { get; set; }
         public string AoName { get; set; }
         public DateTime QDate { get; set; }
-        public bool IsTesting { get; set; } = true;
+        public bool IsTesting { get; set; } 
     }
 }
