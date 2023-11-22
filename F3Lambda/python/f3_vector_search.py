39611f218cdebd7c730d2b61d482323fc622d7b7
Kevin Jones <kevin.jones@cbsinteractive.com>
2023-11-21 21:14:51 -0800

    Python body update

--- F3Lambda/python/f3_vector_search.py
+++ F3Lambda/python/f3_vector_search.py
@@ -2,11 +2,12 @@ import os
 from momento import PreviewVectorIndexClientAsync, VectorIndexConfigurations, CredentialProvider
 import openai
 import asyncio
+import json
 
 async def async_handler(event, context):
     print (event)
-    # Read the term from the event
-    term = event['term']
+    body = event['body']
+    term = json.loads(body)['term']
 
     # Instantiate Momento Vector Client
     mvi_client = PreviewVectorIndexClientAsync(   
@@ -56,9 +57,8 @@ def lambda_handler(event, context):
 
 # if __name__ == "__main__":
 #     # Define a sample event and context
-#     event = {
-#         "term": "animal"  # Replace with your actual event data
-#     }
+#     event = {'version': '2.0', 'routeKey': '$default', 'rawPath': '/', 'rawQueryString': '', 'headers': {'content-length': '26', 'x-amzn-tls-version': 'TLSv1.2', 'x-forwarded-proto': 'https', 'postman-token': '79d096a6-31e7-44d2-9933-4f5c16d596b9', 'x-forwarded-port': '443', 'x-forwarded-for': '73.151.4.230', 'accept': '*/*', 'x-amzn-tls-cipher-suite': 'ECDHE-RSA-AES128-GCM-SHA256', 'x-amzn-trace-id': 'Root=1-655d8d10-60dfe55d02ee04dc1bef7b12', 'host': 'wdd5t63r4ve5btbdv5yypcnugq0fgeow.lambda-url.us-west-1.on.aws', 'content-type': 'application/json', 'accept-encoding': 'gzip, deflate, br', 'user-agent': 'PostmanRuntime/7.35.0'}, 'requestContext': {'accountId': 'anonymous', 'apiId': 'wdd5t63r4ve5btbdv5yypcnugq0fgeow', 'domainName': 'wdd5t63r4ve5btbdv5yypcnugq0fgeow.lambda-url.us-west-1.on.aws', 'domainPrefix': 'wdd5t63r4ve5btbdv5yypcnugq0fgeow', 'http': {'method': 'GET', 'path': '/', 'protocol': 'HTTP/1.1', 'sourceIp': '73.151.4.230', 'userAgent': 'PostmanRuntime/7.35.0'}, 'requestId': '45924bba-8cc8-4646-b5a0-23301f668e04', 'routeKey': '$default', 'stage': '$default', 'time': '22/Nov/2023:05:09:36 +0000', 'timeEpoch': 1700629776665}, 'body': '{\r\n    "term": "animal"\r\n}', 'isBase64Encoded': False}
+
 #     context = {}  # Context can be an empty dictionary when testing
 
 #     # Get an event loop and run the function
