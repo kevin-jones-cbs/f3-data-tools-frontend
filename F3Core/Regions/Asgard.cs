73b759e55ac0b3d7f629273684f4c516ae8a9bca
Kevin Jones <kevin.jones@paramount.com>
2024-05-03 14:13:09 -0700

    Update Asgard locations

--- F3Core/Regions/Asgard.cs
+++ F3Core/Regions/Asgard.cs
@@ -9,16 +9,17 @@ namespace F3Core.Regions
         public override List<Ao> AoList => new List<Ao>
         {
             new Ao { Name = "The Boneyard", City = "Sacramento", DayOfWeek = DayOfWeek.Monday },
-            new Ao { Name = "Training Ground", City = "Antelope", DayOfWeek = DayOfWeek.Monday },
+            new Ao { Name = "Pow Wow - M", City = "Orangevale", DayOfWeek = DayOfWeek.Monday },
             new Ao { Name = "Airstrip", City = "Rancho Cordova", DayOfWeek = DayOfWeek.Tuesday },
-            new Ao { Name = "Pow Wow - T", City = "Orangevale", DayOfWeek = DayOfWeek.Tuesday },
+            new Ao { Name = "Training Ground - T", City = "Antelope", DayOfWeek = DayOfWeek.Tuesday },
             new Ao { Name = "The Mammoth", City = "Sacramento", DayOfWeek = DayOfWeek.Wednesday },
             new Ao { Name = "The Pound - W", City = "Citrus Heights", DayOfWeek = DayOfWeek.Wednesday },
             new Ao { Name = "The Ring - Th", City = "Fair Oaks", DayOfWeek = DayOfWeek.Thursday },
             new Ao { Name = "Sandy Garden - Th", City = "Fair Oaks", DayOfWeek = DayOfWeek.Thursday },
             new Ao { Name = "The Iron Jungle - Fr", City = "Sacramento", DayOfWeek = DayOfWeek.Friday },
             new Ao { Name = "Beavertown", City = "Sacramento", DayOfWeek = DayOfWeek.Friday },
-            new Ao { Name = "The Break", City = "Sacramento", DayOfWeek = DayOfWeek.Saturday }
+            new Ao { Name = "The Break", City = "Sacramento", DayOfWeek = DayOfWeek.Saturday },
+            new Ao { Name = "The Yard", City = "Folsom", DayOfWeek = DayOfWeek.Saturday }
         };
 
         public override string TestingSpreadsheetId => "1lyFtAFDJExSb9pIqh-hJ4AsOHxwR1i84UrjHVNf4bgc";
