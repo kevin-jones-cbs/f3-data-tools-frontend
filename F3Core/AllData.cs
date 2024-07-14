b2f79292aca503f704c043e8a8a45843bde58bb8
Kevin Jones <kevin.jones@cbsinteractive.com>
2024-07-14 13:06:43 -0700

    Remove Core project (moving to its own repo)

--- F3Core/AllData.cs
+++
@@ -1,9 +0,0 @@
-namespace F3Core
-{
-    public class AllData
-    {
-        public List<Post> Posts { get; set; }
-        public List<Pax> Pax { get; set; }
-        public List<Ao> Aos { get; set; }
-    }
-}
